4b1849756fe773c54ba6858bfc273f0cffa6f15d
dyankastutara <dyan.kastutara@gmail.com>
2019-10-15 19:30:18 +0700

    initial state

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const cors = require ('cors');
+
+const app = express();
+
+app.set('port', process.env.PORT || 8081);
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.listen(app.get('port'), ()=>{
+  console.log("Application running at port ", app.get('port'));
+})
